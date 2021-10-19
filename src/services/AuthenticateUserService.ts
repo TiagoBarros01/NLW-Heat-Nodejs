7de1fa47fcb88ed6596ce1ba1bd1053d9d1006a6
Tiago Barros <tbarros1337@hotmail.com>
2021-10-19 08:02:04 -0300

    feat: Authenticate User & user types

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,21 +1,39 @@
 import axios from 'axios';
+import { IUser } from '../@types/User';
+
+interface IAccessTokenResponse {
+  access_token: string;
+  token_type?: string;
+  scope?: string;
+}
+
+type IUserResponse = Pick<IUser, 'avatar_url' | 'login' | 'id' | 'name'>;
 
 export class AuthenticateUserService {
   async execute(code: string) {
     const url = 'https://github.com/login/oauth/access_token';
 
-    const { data } = await axios.post(url, null, {
+    const { data } = await axios.post<IAccessTokenResponse>(url, null, {
       params: {
         client_id: process.env.GITHUB_CLIENT_ID,
         client_secret: process.env.GITHUB_CLIENT_SECRET,
         code,
       },
-      
+
       headers: {
         Accept: 'application/json',
       },
     });
 
-    return data;
+    const { data: responseUser } = await axios.get<IUserResponse>(
+      'https://api.github.com/user',
+      {
+        headers: {
+          authorization: `Bearer ${data.access_token}`,
+        },
+      }
+    );
+
+    return responseUser;
   }
 }
