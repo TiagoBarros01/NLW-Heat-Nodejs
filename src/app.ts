b49ab57f9a303866bb5999a01bdbc04be7601d4c
Tiago Barros <tbarros1337@hotmail.com>
2021-10-19 07:27:52 -0300

    feat: Creates routes & add dotenv

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,18 @@
-import express from 'express';
+import 'dotenv/config';
+import express, { Request, Response } from 'express';
 
 const app = express();
 
 app.listen(4000, () => console.log('🔥 Server is running at port 4000'));
+
+app.get('/auth', (req: Request, res: Response) =>
+  res.redirect(
+    `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`
+  )
+);
+
+app.get('/signin/callback', (req: Request, res: Response) => {
+  const { code } = req.query;
+
+  return res.json(code)
+});
